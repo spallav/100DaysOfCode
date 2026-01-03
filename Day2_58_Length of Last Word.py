ca6f5dfbb5ba707c5525bc902c4652c819ff8905
spallav <79427047+spallav@users.noreply.github.com>
2026-01-02 23:18:32 -0800

    Day 2 / Day 100
    Day2_58_Length of Last Word

--- Day2_58_Length of Last Word.py
+++ Day2_58_Length of Last Word.py
@@ -1,5 +1,5 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         s = s.strip()               # Remove leading/trailing spaces
-        last_word = s.split()[-1]   # Split into words and take the last one
+        last_word = s.split()[-1]   # Split into words and take the lastone
         return len(last_word)
